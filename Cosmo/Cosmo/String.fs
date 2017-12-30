6e3bde834d0d947978f91241d8531bd8056bcafd
Sean Batten <seanbatten58@gmail.com>
2017-12-30 15:11:39 +0000

    Initial code

---
+++ Cosmo/Cosmo/String.fs
@@ -0,0 +1,31 @@
+﻿module Cosmo.String
+
+open System.Text
+
+/// Spit a string
+let Split (separator : char) (text : string) =
+    text.Split [| separator |] |> Array.toList 
+
+/// Join a sequence of strings
+let Join (separator : string) (strings : string seq) =
+    System.String.Join (separator, Seq.toArray strings)
+
+/// Determines if a string is null or empty
+let IsNullOrEmpty text =
+    System.String.IsNullOrEmpty text
+
+/// Determines if a string is null or whitespace
+let IsNullOrWhiteSpace text =
+    System.String.IsNullOrWhiteSpace text
+
+/// Converts a string to lower case
+let ToLower (text : string) =
+    text.ToLower()
+
+/// Converts a string to upper case
+let ToUpper (text : string) =
+    text.ToUpper()
+
+/// Trims excess whitespace
+let Trim (text : string) =
+    text.Trim()
---
+++ Cosmo/CosmoConsole/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Cosmo.String;
+
+[<EntryPoint>]
+let main argv = 
+    let greeting = [| "hello"; "world    "|] |> Join "," |> Trim
+    printfn "%A" greeting
+    0 // return an integer exit code
